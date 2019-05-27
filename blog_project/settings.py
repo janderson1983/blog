516e0d14a139574353fef4abf15bbbfd8d37e331
John Anderson <janderson1983@gmail.com>
2019-05-27 16:58:30 -0400

    HEroku COnfig

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1hhk@k0&7lhu@tg1)_5ou1lyg^@vdik$jn+)sj%w+0zjudsa9b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
